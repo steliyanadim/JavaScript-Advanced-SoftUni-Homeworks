495b821e9b7ec52529ba74793da2f92417880a89
steliyanadim <steliyanadim@gmail.com>
2023-01-04 09:22:30 +0200

    Exercise: Syntax, Functions and Statements

---
+++ Syntax, Functions and Statements/Syntax, Functions and Statements - Lab/stringLength.js
@@ -0,0 +1,7 @@
+function stringLength(first, second, third){
+let sum = first.length + second.length + third.length;
+let average = Math.floor(sum / 3);
+console.log(sum);
+console.log(average);
+}
+stringLength('pasta', '5', '22.3')
